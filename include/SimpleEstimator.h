69b8aa25cd66fdefedf0115a52d1cc596c705e36
jacordero <colli.cordero@gmail.com>
2018-03-02 20:46:30 +0100

    basic random sampling version for the estimator

--- include/SimpleEstimator.h
+++ include/SimpleEstimator.h
@@ -8,16 +8,27 @@
 #include "Estimator.h"
 #include "SimpleGraph.h"
 
+enum EstimatorType {
+	naive,
+	simpleSampling,
+	radu,
+	bodgan
+};
+
 class SimpleEstimator : public Estimator {
 
+	// used by everyone
 	std::shared_ptr<SimpleGraph> graph;
+	EstimatorType estimatorType;
 
-	// matrices used to perform the cardinality estimation
+	// used by naive
 	std::vector<uint32_t> incoming_labels;
 	std::vector<uint32_t> outgoing_labels;
 	uint32_t smaller_number_labels;
 	uint32_t larger_number_labels;
 	uint32_t length_query;
+
+	// used by simple random sampling
 
 
 public:
@@ -26,19 +37,26 @@ public:
 
 	void prepare() override;
 	cardStat estimate(RPQTree *q) override;
+
+	/** used by the naive estimator **/
+	void prepareNaive();
+	cardStat estimateNaive(RPQTree *q);
 	void iterateRPQTree(RPQTree *q);
 	bool onlyDigits(std::string str);
 
-	void constructorJorge(std::shared_ptr<SimpleGraph> &g);
-	void prepareJorge();
-	cardStat estimateJorge(RPQTree *q);
-	void destructorJorge();
+	/** used by random sampling estimator **/
+	void prepForRandomSamplingEstimator(std::shared_ptr<SimpleGraph> &synopsis);
+	void addEdgesByRandomWalk(std::shared_ptr<SimpleGraph> &synopsis,
+							  float percentage_to_keep, int no_edges);
+	cardStat estimateByRandomSampling(RPQTree *q);
 
+	/** used by [name] **/
 	void constructorRadu(std::shared_ptr<SimpleGraph> &g);
 	void prepareRadu();
 	cardStat estimateRadu(RPQTree *q);
 	void destructorRadu();
 
+	/** used by [name] **/
 	void constructorBogdan(std::shared_ptr<SimpleGraph> &g);
 	void prepareBogdan();
 	cardStat estimateBogdan(RPQTree *q);
--- src/SimpleEstimator.cpp
+++ src/SimpleEstimator.cpp
@@ -4,27 +4,71 @@
 
 #include "SimpleGraph.h"
 #include "SimpleEstimator.h"
+#include "SimpleEvaluator.h"
+
 
 SimpleEstimator::SimpleEstimator(std::shared_ptr<SimpleGraph> &g) {
-	constructorJorge(g);
-}
-
-void SimpleEstimator::constructorJorge(std::shared_ptr<SimpleGraph> &g)
-{
-	// works only with SimpleGraph
+	// variables used by everyone
 	graph = g;
+	estimatorType = simpleSampling;
+
+
+	// ************ variables used by jc's code ******
+	// used for naive estimator implementation
 	incoming_labels = std::vector<uint32_t>(0);
 	outgoing_labels = std::vector<uint32_t >(0);
-	//length_query
+
+	// used for random sampling estimator
+
+	// ************* variables used by radu's code ***********
+
+	// ************* variables used by bodgan's code ************
 }
 
-void SimpleEstimator::prepareJorge()
-{
+
+void SimpleEstimator::prepare() {
+	// do your prep here
+	switch(estimatorType){
+		case naive:
+			prepareNaive();
+			break;
+		case simpleSampling:
+
+			break;
+
+		case radu:
+			break;
+
+		case bodgan:
+			break;
+	}
+}
+
+
+cardStat SimpleEstimator::estimate(RPQTree *q){
+	cardStat result = cardStat{0,0,0};
+	switch(estimatorType){
+		case naive:
+			result = estimateNaive(q);
+		case simpleSampling:
+			result = estimateByRandomSampling(q);
+			break;
+		case radu:
+			break;
+		case bodgan:
+			break;
+	}
+	return result;
+}
+
+
+
+void SimpleEstimator::prepareNaive() {
 	// do your prep here
 	// play a bit with the graph data structure
-	std::cout << "Number of labels for the given graph: " << graph->getNoLabels() << std::endl;
-	std::cout << "Number of vertices for the given graph: " << graph->getNoVertices() << std::endl;
-	std::cout << "Number of edges for the given graph: " << graph->getNoEdges() << std::endl;
+	//std::cout << "Number of labels for the given graph: " << graph->getNoLabels() << std::endl;
+	//std::cout << "Number of vertices for the given graph: " << graph->getNoVertices() << std::endl;
+	//std::cout << "Number of edges for the given graph: " << graph->getNoEdges() << std::endl;
 
 	int no_labels = graph->getNoLabels();
 
@@ -47,8 +91,7 @@ void SimpleEstimator::prepareJorge()
 
 }
 
-cardStat SimpleEstimator::estimateJorge(RPQTree *q)
-{
+cardStat SimpleEstimator::estimateNaive(RPQTree *q) {
 	q->print();
 	// perform your estimation here
 
@@ -58,28 +101,132 @@ cardStat SimpleEstimator::estimateJorge(RPQTree *q)
 	length_query = 0;
 	// iterate
 
-	std::cout << "Before estimation process" << std::endl;
-	std::cout << "Smaller number of labels: " << smaller_number_labels << std::endl;
-	std::cout << "Larger number of labels: " << larger_number_labels << std::endl;
+	//std::cout << "Before estimation process" << std::endl;
+	//std::cout << "Smaller number of labels: " << smaller_number_labels << std::endl;
+	//std::cout << "Larger number of labels: " << larger_number_labels << std::endl;
 
 	// iterate over RPQTree data
 	iterateRPQTree(q);
 	//iterateRPQTree(q->righ)
 
-	std::cout << "After estimation process" << std::endl;
-	std::cout << "Smaller number of labels: " << smaller_number_labels << std::endl;
-	std::cout << "Larger number of labels: " << larger_number_labels << std::endl;
+	//std::cout << "After estimation process" << std::endl;
+	//std::cout << "Smaller number of labels: " << smaller_number_labels << std::endl;
+	//std::cout << "Larger number of labels: " << larger_number_labels << std::endl;
 
 	// noOut, noPaths, noIn
-	return cardStat{ length_query, smaller_number_labels * larger_number_labels, length_query };
-
-	return cardStat{ 0, 0, 0 };
+	return cardStat{length_query, smaller_number_labels * larger_number_labels, length_query};
 }
 
-void SimpleEstimator::destructorJorge()
-{
 
+
+void SimpleEstimator::iterateRPQTree(RPQTree *q) {
+
+	if (q->isLeaf()) {
+		char c = (q->data).at(0);
+		std::string str = std::string(1, c);
+		if (onlyDigits(str)) {
+			int label_id = std::stoi(str, nullptr);
+			length_query++;
+			int no_labels = incoming_labels[label_id];
+			if (no_labels > larger_number_labels) {
+				larger_number_labels = no_labels;
+			}
+			if (no_labels < smaller_number_labels) {
+				smaller_number_labels = no_labels;
+			}
+		}
+	}
+	else {
+		if (q->left != nullptr) iterateRPQTree(q->left);
+		if (q->right != nullptr) iterateRPQTree(q->right);
+	}
 }
+
+bool SimpleEstimator::onlyDigits(std::string str) {
+	return (str.find_first_not_of("0123456789") == std::string::npos);
+}
+
+void SimpleEstimator::prepForRandomSamplingEstimator(std::shared_ptr<SimpleGraph> &synopsis){
+	//std::cout << "Starting prepareForSimpleSamplingEstimator" << std::endl;
+	float percentage_to_keep = 0.2;
+	addEdgesByRandomWalk(synopsis, percentage_to_keep, graph->getNoEdges());
+}
+
+void SimpleEstimator::addEdgesByRandomWalk(std::shared_ptr<SimpleGraph> &synopsis,
+										   float percentage_to_keep, int no_edges){
+	//SimpleGraph synopsis = SimpleGraph();
+	// force to resize the graph
+	synopsis->setNoVertices(0);
+
+	//std::cout << "Entering selectSubgraphByRandomWalk" << std::endl;
+	uint32_t no_selected_edges = 0;
+	uint32_t edges_to_select = std::floor(percentage_to_keep * no_edges);
+	//std::cout << "Setting values for synopsis"<< std::endl;
+	//synopsis.s
+	synopsis->setNoVertices(graph->getNoVertices());
+	synopsis->setNoLabels(graph->getNoLabels());
+
+	//std::cout << "Number of edges: " << no_edges << std::endl;
+	//std::cout << "Percentage of edges to keep: " << percentage_to_keep << std::endl;
+	//std::cout << "Number of edges to select: " << edges_to_select << std::endl;
+
+	std::srand(std::time(nullptr)); // use current time as seed for random generator
+	int current_node = std::rand() % (graph->getNoVertices());
+
+	// teleport parameter
+	float alpha = 1 / graph->getNoVertices();
+
+	// select random node to start the walking
+	while (no_selected_edges < edges_to_select){
+
+		if (std::rand() < alpha){
+			// teleport by change
+			current_node = std::rand() % (graph->getNoVertices());
+		} else if (graph->adj[current_node].empty()){
+			// force teleportation
+			current_node = std::rand() % (graph->getNoVertices());
+		} else if (synopsis->adj[current_node].size() >= graph->adj[current_node].size()){
+			// force teleportation because we already processed all edges here
+			current_node = std::rand() % (graph->getNoVertices());
+		} else {
+			// here we allow duplication
+			int next_pair_pos = std::rand() % (graph->adj[current_node].size());
+			std::pair<uint32_t, uint32_t> selected_pair = graph->adj[current_node][next_pair_pos];
+			synopsis->addEdge(current_node, selected_pair.second, selected_pair.first);
+			no_selected_edges++;
+		}
+
+	}
+
+	//std::cout << "Number of selected edges: " << synopsis->getNoEdges() << std::endl;
+	//std::cout << "Synopsis construction is finished" << std::endl;
+}
+
+cardStat SimpleEstimator::estimateByRandomSampling(RPQTree *q) {
+	//std::cout << "Running simpleSamplingEstimator " << std::endl;
+	auto synopsis = std::make_shared<SimpleGraph>();
+	int reduction_factor = 10;
+	float percentage_to_keep = 1.0 / reduction_factor;
+
+	addEdgesByRandomWalk(synopsis, percentage_to_keep, graph->getNoEdges());
+	//prepareForSimpleSamplingEstimator(synopsis);
+	SimpleEvaluator eval = SimpleEvaluator(synopsis);
+	cardStat firstStats = eval.evaluate(q);
+	firstStats.noPaths = firstStats.noPaths*reduction_factor;
+	// resize the synopsis object
+	//delete &synopsis;
+
+	addEdgesByRandomWalk(synopsis, percentage_to_keep, graph->getNoEdges());
+	//prepareForSimpleSamplingEstimator(synopsis);
+	eval = SimpleEvaluator(synopsis);
+	cardStat secondStats = eval.evaluate(q);
+	secondStats.noPaths = secondStats.noPaths*reduction_factor;
+
+	int avgNoPaths = 0.5*(firstStats.noPaths + secondStats.noPaths);
+
+	return cardStat{0, avgNoPaths, 0};
+}
+
 
 void SimpleEstimator::constructorRadu(std::shared_ptr<SimpleGraph> &g)
 {
@@ -121,38 +268,4 @@ void SimpleEstimator::destructorBogdan()
 
 }
 
-void SimpleEstimator::prepare() {
-	prepareJorge();
-}
 
-cardStat SimpleEstimator::estimate(RPQTree *q) {
-	return estimateJorge(q);
-}
-
-
-void SimpleEstimator::iterateRPQTree(RPQTree *q) {
-
-	if (q->isLeaf()) {
-		char c = (q->data).at(0);
-		std::string str = std::string(1, c);
-		if (onlyDigits(str)) {
-			int label_id = std::stoi(str, nullptr);
-			length_query++;
-			int no_labels = incoming_labels[label_id];
-			if (no_labels > larger_number_labels) {
-				larger_number_labels = no_labels;
-			}
-			if (no_labels < smaller_number_labels) {
-				smaller_number_labels = no_labels;
-			}
-		}
-	}
-	else {
-		if (q->left != nullptr) iterateRPQTree(q->left);
-		if (q->right != nullptr) iterateRPQTree(q->right);
-	}
-}
-
-bool SimpleEstimator::onlyDigits(std::string str) {
-	return (str.find_first_not_of("0123456789") == std::string::npos);
-}
